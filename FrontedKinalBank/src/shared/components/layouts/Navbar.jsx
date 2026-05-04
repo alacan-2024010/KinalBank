f60747ecd9e7d8aabf0144ffbabbe8c5b52b5873
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-05-04 15:44:37 -0600

    Se corrigio la ruta en el navbar

--- FrontedKinalBank/src/shared/components/layouts/Navbar.jsx
+++ FrontedKinalBank/src/shared/components/layouts/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import { useAuthStore } from "../../features/auth/store/authStore"
+import { useAuthStore } from "../../../features/auth/store/authStore.js"
 
 export const Navbar = () => {
     const navigate = useNavigate()
