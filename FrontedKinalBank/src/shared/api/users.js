3cfb4f1a87afd446a7113c41bf46696a38722772
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-05-04 17:15:16 -0600

    Se agregaron las rutas definidas en el backend

--- FrontedKinalBank/src/shared/api/users.js
+++ FrontedKinalBank/src/shared/api/users.js
@@ -1,8 +1,13 @@
-
 import { axiosAuth } from "./api";
 
 export const getPendingUsers = () =>
   axiosAuth.get("/users/pending");
 
+export const getApprovedUsers = (search = "") =>
+  axiosAuth.get(`/users/approved?search=${encodeURIComponent(search)}`);
+
 export const approveUser = (userId, role = "CLIENT") =>
   axiosAuth.put("/users/approve", { userId, role });
+
+export const denyUser = (userId) =>
+  axiosAuth.delete(`/users/deny/${userId}`);
