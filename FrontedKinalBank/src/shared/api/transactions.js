f08d3046cfbf32c230beda0f557e0c4f830f71dd
efolgar-2021545 <efolgar-2021545@kinal.edu.gt>
2026-05-07 19:28:22 -0600

    Se agrego la ruta de my-transactions

--- FrontedKinalBank/src/shared/api/transactions.js
+++ FrontedKinalBank/src/shared/api/transactions.js
@@ -1,7 +1,10 @@
 import { coreApi } from "./api.js";
-
+ 
 export const makeTransfer = (data) =>
     coreApi.post("/transactions/create", data);
-
+ 
 export const getTransactionById = (id) =>
     coreApi.get(`/transactions/${id}`);
+ 
+export const getMyTransactions = (page = 1) =>
+    coreApi.get(`/transactions/my-transactions?page=${page}`);
